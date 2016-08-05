d69e35daa57598e50004b2a74103db33e68469be
Jonathan Cox <joncox@gmail.com>
2016-08-05 08:19:54 -0400

    abstract petition form for create and edit

--- resources/views/petition/create.blade.php
+++ resources/views/petition/create.blade.php
@@ -9,105 +9,10 @@
                 <div class="panel-heading">Create a Petition</div>
 
                 <div class="panel-body">
-                    <form action="{{ action('PetitionController@store') }}" method="POST">
-                        {{ csrf_field() }}
-
-                        <div>
-                          <!-- Nav tabs -->
-                          <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#details" aria-controls="details" role="tab" data-toggle="tab">Details</a></li>
-                            <li role="presentation"><a href="#thanks" aria-controls="thanks" role="tab" data-toggle="tab">Thanks</a></li>
-                            <li role="presentation"><a href="#email" aria-controls="email" role="tab" data-toggle="tab">Email Response</a></li>
-                            
-                            <li class="pull-right">
-                                <button type="submit" class="btn btn-primary">
-                                    Create
-                                </button>
-                            </li>
-                            
-                          </ul>
-                          
-                          <div>
-                              <br><!-- TODO: replace with css adjustments --> 
-                          </div>
-
-                          <!-- Tab panes -->
-                          <div class="tab-content">
-                            <div role="tabpanel" class="tab-pane fade in active" id="details">
-                                @include('forms.controls.input', [
-                                  'name' => 'title',
-                                  'label' => 'Title',
-                                  'value' => '',
-                                  'attributes' => [
-                                      'placeholder' => 'enter clever title here'
-                                  ]
-                                ])
-                                
-                                @include('forms.controls.checkbox', [
-                                  'name' => 'published',
-                                  'label' => 'published',
-                                  'type' => 'checkbox',
-                                  'value' => ''
-                                ])
-
-                                @include('forms.controls.textarea', [
-                                  'name' => 'summary',
-                                  'label' => 'Summary',
-                                  'value' => '',
-                                  'attributes' => [
-                                      'placeholder' => 'type a simple summary'
-                                  ]
-                                ])
-
-                                @include('forms.controls.textarea', [
-                                  'name' => 'body',
-                                  'label' => 'Body',
-                                  'value' => '',
-                                  'attributes' => [
-                                      'placeholder' => 'give as much detail as you can but be concise',
-                                      'rows' => 15
-                                  ]
-                                ])
-                                
-                            </div>
-                            
-                            <div role="tabpanel" class="tab-pane fade" id="thanks">
-                                @include('forms.controls.textarea', [
-                                  'name' => 'thanks_message',
-                                  'label' => 'Thank You Message',
-                                  'value' => '',
-                                  'attributes' => [
-                                      'placeholder' => 'say something nice to your signees',
-                                      'rows' => 15
-                                  ]
-                                ])
-                            </div>
-                            
-                            <div role="tabpanel" class="tab-pane fade" id="email">
-                                @include('forms.controls.input', [
-                                  'name' => 'thanks_email_subject',
-                                  'label' => 'Subject',
-                                  'value' => '',
-                                  'attributes' => [
-                                      'placeholder' => ''
-                                  ]
-                                ])
-                                
-                                @include('forms.controls.textarea', [
-                                  'name' => 'thanks_email_body',
-                                  'label' => 'Body',
-                                  'value' => '',
-                                  'attributes' => [
-                                      'placeholder' => '',
-                                      'rows' => 15
-                                  ]
-                                ])
-                            </div>
-                          </div>
-
-                        </div>
-                        
-                    </form>
+                    @include('petition.form', [
+                        'action_url' => action('PetitionController@store'),
+                        'action_name' => 'Create'
+                    ])
                 </div>
             </div>
         </div>
---
+++ resources/views/petition/form.blade.php
@@ -0,0 +1,99 @@
+<form action="{{ $action_url }}" method="POST">
+    {{ csrf_field() }}
+
+    <div>
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#details" aria-controls="details" role="tab" data-toggle="tab">Details</a></li>
+        <li role="presentation"><a href="#thanks" aria-controls="thanks" role="tab" data-toggle="tab">Thanks</a></li>
+        <li role="presentation"><a href="#email" aria-controls="email" role="tab" data-toggle="tab">Email Response</a></li>
+        
+        <li class="pull-right">
+            <button type="submit" class="btn btn-primary">
+                {{ $action_name }}
+            </button>
+        </li>
+        
+      </ul>
+      
+      <div>
+          <br><!-- TODO: replace with css adjustments --> 
+      </div>
+
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane fade in active" id="details">
+            @include('forms.controls.input', [
+              'name' => 'title',
+              'label' => 'Title',
+              'value' => '',
+              'attributes' => [
+                  'placeholder' => 'enter clever title here'
+              ]
+            ])
+            
+            @include('forms.controls.checkbox', [
+              'name' => 'published',
+              'label' => 'published',
+              'type' => 'checkbox',
+              'value' => ''
+            ])
+
+            @include('forms.controls.textarea', [
+              'name' => 'summary',
+              'label' => 'Summary',
+              'value' => '',
+              'attributes' => [
+                  'placeholder' => 'type a simple summary'
+              ]
+            ])
+
+            @include('forms.controls.textarea', [
+              'name' => 'body',
+              'label' => 'Body',
+              'value' => '',
+              'attributes' => [
+                  'placeholder' => 'give as much detail as you can but be concise',
+                  'rows' => 15
+              ]
+            ])
+            
+        </div>
+        
+        <div role="tabpanel" class="tab-pane fade" id="thanks">
+            @include('forms.controls.textarea', [
+              'name' => 'thanks_message',
+              'label' => 'Thank You Message',
+              'value' => '',
+              'attributes' => [
+                  'placeholder' => 'say something nice to your signees',
+                  'rows' => 15
+              ]
+            ])
+        </div>
+        
+        <div role="tabpanel" class="tab-pane fade" id="email">
+            @include('forms.controls.input', [
+              'name' => 'thanks_email_subject',
+              'label' => 'Subject',
+              'value' => '',
+              'attributes' => [
+                  'placeholder' => ''
+              ]
+            ])
+            
+            @include('forms.controls.textarea', [
+              'name' => 'thanks_email_body',
+              'label' => 'Body',
+              'value' => '',
+              'attributes' => [
+                  'placeholder' => '',
+                  'rows' => 15
+              ]
+            ])
+        </div>
+      </div>
+
+    </div>
+    
+</form>
